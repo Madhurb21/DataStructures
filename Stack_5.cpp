08989b528a5bac19cfe152f68afe48a50f5d55db
Madhurb21 <madhurb21@iitk.ac.in>
2022-07-22 11:24:42 +0530

    Stack but infix, postfix is pending

---
+++ Stack_5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <stack>
+
+int evaluate(std::string expression, int len)
+{
+
+}
+
+int main()
+{
+    std::string expression;
+    char* str[50];
+    int ans;
+
+    std::cout << "Enter an expression - ";
+    std::cin >> expression;
+    ans = evaluate(expression, expression.length());
+}
